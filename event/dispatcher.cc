ed6d0536ec6ff8396618acf36b5349a7ed9d40d9
Donald King <chronos@chronos-tachyon.net>
2016-12-28 19:14:19 -0800

    Refactor threaded dispatcher to split members over two mutexes.

--- event/dispatcher.cc
+++ event/dispatcher.cc
@@ -40,14 +40,14 @@ static void invoke(base::Lock& lock, std::size_t& busy, std::size_t& done,
                    std::size_t& caught, Work item) noexcept {
   bool threw = true;
   ++busy;
-  auto cleanup0 = base::cleanup([&busy, &done, &caught, &threw] {
+  auto cleanup = base::cleanup([&busy, &done, &caught, &threw] {
     --busy;
     ++done;
     if (threw) ++caught;
   });
 
   lock.unlock();
-  auto cleanup1 = base::cleanup([&lock] { lock.lock(); });
+  auto reacquire = base::cleanup([&lock] { lock.lock(); });
 
   if (item.task == nullptr || item.task->start()) {
     try {
@@ -72,7 +72,7 @@ static void invoke(base::Lock& lock, IdleFunction idle) noexcept {
   if (!idle) return;
 
   lock.unlock();
-  auto cleanup = base::cleanup([&lock] { lock.lock(); });
+  auto reacquire = base::cleanup([&lock] { lock.lock(); });
 
   try {
     idle();
@@ -192,13 +192,8 @@ class ThreadPoolDispatcher : public Dispatcher {
         done_(0),
         caught_(0),
         corked_(false) {
-    if (min > max) {
-      LOG(DFATAL) << "BUG: min > max";
-      max = min;
-    }
-    auto lock = base::acquire_lock(mu_);
-    ensure();
-    while (current_ < min_) curr_cv_.wait(lock);
+    auto lock1 = base::acquire_lock(mu1_);
+    ensure(lock1);
   }
 
   ~ThreadPoolDispatcher() noexcept override { shutdown(); }
@@ -208,23 +203,26 @@ class ThreadPoolDispatcher : public Dispatcher {
   }
 
   void dispatch(Task* task, CallbackPtr callback) override {
-    auto lock = base::acquire_lock(mu_);
+    auto lock0 = base::acquire_lock(mu0_);
+    std::size_t n = work_.size();
     work_.emplace_back(task, std::move(callback));
     if (corked_) return;
+    work_cv_.notify_one();
+    lock0.unlock();
 
-    std::size_t n = work_.size();
     // HEURISTIC: if queue size is greater than num threads, add a thread.
     //            (Threads that haven't finished starting add to the count.)
     //            This is (intentionally) a fairly aggressive growth policy.
-    if (desired_ < max_ && n >= desired_) {
+    auto lock1 = base::acquire_lock(mu1_);
+    if (desired_ < max_ && n > desired_) {
       ++desired_;
-      ensure();
+      ensure(lock1);
     }
-    work_cv_.notify_one();
   }
 
   DispatcherStats stats() const noexcept override {
-    auto lock = base::acquire_lock(mu_);
+    auto lock0 = base::acquire_lock(mu0_);
+    auto lock1 = base::acquire_lock(mu1_);
     DispatcherStats tmp;
     tmp.min_workers = min_;
     tmp.max_workers = max_;
@@ -239,121 +237,160 @@ class ThreadPoolDispatcher : public Dispatcher {
   }
 
   void shutdown() noexcept override {
-    auto lock = base::acquire_lock(mu_);
+    auto lock1 = base::acquire_lock(mu0_);
     min_ = max_ = desired_ = 0;
-    while (current_ > desired_) curr_cv_.wait(lock);
+    ensure(lock1);
   }
 
   base::Result adjust(const DispatcherOptions& opts) noexcept override {
-    auto lock = base::acquire_lock(mu_);
     std::size_t min, max;
     bool has_min, has_max;
     std::tie(has_min, min) = opts.min_workers();
     std::tie(has_max, max) = opts.max_workers();
+
+    auto lock1 = base::acquire_lock(mu1_);
     if (!has_min) min = min_;
     if (!has_max) max = std::max(min, max_);
     if (min > max)
       return base::Result::invalid_argument(
           "bad event::DispatcherOptions: min_workers > max_workers");
+
     min_ = min;
     max_ = max;
     if (desired_ < min_) desired_ = min_;
     if (desired_ > max_) desired_ = max_;
-    ensure();
-    // Block until the thread count is within the new bounds.
-    while (current_ < min_) curr_cv_.wait(lock);
-    while (current_ > max_) curr_cv_.wait(lock);
+    ensure(lock1);
     return base::Result();
   }
 
   void cork() noexcept override {
-    auto lock = base::acquire_lock(mu_);
+    auto lock0 = base::acquire_lock(mu0_);
     CHECK(!corked_);
     corked_ = true;
-    while (busy_ > 0) busy_cv_.wait(lock);
+    while (busy_ != 0) busy_cv_.wait(lock0);
   }
 
   void uncork() noexcept override {
-    auto lock = base::acquire_lock(mu_);
+    auto lock0 = base::acquire_lock(mu0_);
     CHECK(corked_);
     corked_ = false;
     std::size_t n = work_.size();
-    n = std::min(n, max_);
-    // HEURISTIC: when uncorking, aggressively spawn 1 thread per callback.
-    if (n > desired_) {
-      desired_ = n;
-      ensure();
-    }
     if (n > 1)
       work_cv_.notify_all();
     else if (n == 1)
       work_cv_.notify_one();
+    lock0.unlock();
+
+    auto lock1 = base::acquire_lock(mu1_);
+    n = std::min(n, max_);
+    // HEURISTIC: when uncorking, aggressively spawn 1 thread per callback.
+    if (n > desired_) {
+      desired_ = n;
+      ensure(lock1);
+    }
   }
 
   void donate(bool forever) noexcept override {
     internal::assert_depth();
-    auto lock = base::acquire_lock(mu_);
+    auto lock0 = base::acquire_lock(mu0_);
     if (forever)
-      donate_forever(lock);
+      donate_forever(lock0);
     else
-      donate_once(lock);
+      donate_once(lock0);
   }
 
  private:
+  struct Monitor {
+    ThreadPoolDispatcher* d;
+    bool is_live;
+
+    Monitor(const Monitor&) = delete;
+    Monitor(Monitor&&) = delete;
+    Monitor& operator=(const Monitor&) = delete;
+    Monitor& operator=(Monitor&&) = delete;
+
+    explicit Monitor(ThreadPoolDispatcher* d) noexcept : d(d), is_live(false) {
+      auto lock1 = base::acquire_lock(d->mu1_);
+      inc(lock1);
+    }
+
+    ~Monitor() noexcept {
+      auto lock1 = base::acquire_lock(d->mu1_);
+      if (is_live) dec(lock1);
+    }
+
+    bool maybe_exit() noexcept {
+      auto lock1 = base::acquire_lock(d->mu1_);
+      if (d->current_ > d->desired_) {
+        dec(lock1);
+        return true;
+      }
+      return false;
+    }
+
+    bool too_many() noexcept {
+      auto lock1 = base::acquire_lock(d->mu1_);
+      if (d->desired_ > d->min_) {
+        --d->desired_;
+        dec(lock1);
+        return true;
+      }
+      return false;
+    }
+
+    void inc(base::Lock& lock1) noexcept {
+      CHECK(!is_live);
+      is_live = true;
+      ++d->current_;
+      if (d->current_ == d->desired_) d->curr_cv_.notify_all();
+    }
+
+    void dec(base::Lock& lock1) noexcept {
+      CHECK(is_live);
+      is_live = false;
+      --d->current_;
+      if (d->current_ == d->desired_) d->curr_cv_.notify_all();
+    }
+  };
+
   bool has_work() { return !corked_ && !work_.empty(); }
 
-  void donate_once(base::Lock& lock) noexcept {
+  void donate_once(base::Lock& lock0) noexcept {
     Work item;
     while (has_work()) {
       item = std::move(work_.front());
       work_.pop_front();
       ++l_depth;
       auto cleanup = base::cleanup([] { --l_depth; });
-      invoke(lock, busy_, done_, caught_, std::move(item));
+      invoke(lock0, busy_, done_, caught_, std::move(item));
     }
     if (busy_ == 0) busy_cv_.notify_all();
-    invoke(lock, idle_);
+    invoke(lock0, idle_);
   }
 
-  bool inc_current() noexcept {
-    if (current_ >= desired_) return true;
-    ++current_;
-    curr_cv_.notify_all();
-    return false;
-  }
-
-  void dec_current() noexcept {
-    --current_;
-    curr_cv_.notify_all();
-  }
-
-  bool should_exit() noexcept { return current_ > desired_; }
-
-  void donate_forever(base::Lock& lock) noexcept {
+  void donate_forever(base::Lock& lock0) noexcept {
     using MS = std::chrono::milliseconds;
     static constexpr MS kInitialTimeout = MS(125);
     static constexpr MS kMaximumTimeout = MS(8000);
 
-    if (inc_current()) return;
-    auto cleanup0 = base::cleanup([this] { dec_current(); });
-
+    Monitor monitor(this);
     Work item;
     MS ms(kInitialTimeout);
     while (true) {
       while (has_work()) {
-        if (should_exit()) return;
+        if (monitor.maybe_exit()) return;
         ms = kInitialTimeout;
         item = std::move(work_.front());
         work_.pop_front();
         ++l_depth;
-        auto cleanup1 = base::cleanup([] { --l_depth; });
-        invoke(lock, busy_, done_, caught_, std::move(item));
+        auto cleanup = base::cleanup([] { --l_depth; });
+        invoke(lock0, busy_, done_, caught_, std::move(item));
       }
       if (busy_ == 0) busy_cv_.notify_all();
-      if (should_exit()) return;
-      invoke(lock, idle_);
+      if (monitor.maybe_exit()) return;
+      invoke(lock0, idle_);
       if (has_work()) continue;
-      if (work_cv_.wait_for(lock, ms) == std::cv_status::timeout) {
+      if (work_cv_.wait_for(lock0, ms) == std::cv_status::timeout) {
         // HEURISTIC: If we've waited too long (approx. 2*kMaximumTimeout) with
         //            no work coming from the queue, then reduce the num
         //            threads by one.
@@ -365,38 +402,58 @@ class ThreadPoolDispatcher : public Dispatcher {
         //            pruned once sufficient time has passed.
         if (ms < kMaximumTimeout) {
           ms *= 2;
-        } else {
-          if (desired_ > min_) {
-            --desired_;
-            return;
-          }
+        } else if (monitor.too_many()) {
+          return;
         }
       }
     }
   }
 
-  void ensure() {
-    auto closure = [this] { donate(true); };
-    for (std::size_t i = current_, j = desired_; i < j; ++i) {
-      std::thread t(closure);
-      t.detach();
+  void ensure(base::Lock& lock1) {
+    CHECK_LE(min_, max_);
+    CHECK_LE(min_, desired_);
+    CHECK_LE(desired_, max_);
+
+    if (current_ < desired_) {
+      // Spin up new threads.
+      std::size_t delta = desired_ - current_;
+      while (delta != 0) {
+        auto closure = [this] { donate(true); };
+        std::thread(closure).detach();
+        --delta;
+      }
+    } else if (current_ > desired_) {
+      // Wake up existing threads to self-terminate.
+      lock1.unlock();
+      auto reacquire = base::cleanup([&lock1] { lock1.lock(); });
+      auto lock0 = base::acquire_lock(mu0_);
+      work_cv_.notify_all();
     }
+
+    // Block until the thread count has stabilized.
+    while (current_ != desired_) curr_cv_.wait(lock1);
+
+    CHECK_LE(min_, max_);
+    CHECK_LE(min_, desired_);
+    CHECK_LE(desired_, max_);
+    CHECK_EQ(desired_, current_);
   }
 
-  mutable std::mutex mu_;
-  std::condition_variable work_cv_;
-  std::condition_variable curr_cv_;
-  std::condition_variable busy_cv_;
-  std::deque<Work> work_;
-  IdleFunction idle_;
-  std::size_t min_;
-  std::size_t max_;
-  std::size_t desired_;
-  std::size_t current_;
-  std::size_t busy_;
-  std::size_t done_;
-  std::size_t caught_;
-  bool corked_;
+  mutable std::mutex mu0_;
+  mutable std::mutex mu1_;
+  std::condition_variable work_cv_;  // mu0_: !work_.empty()
+  std::condition_variable busy_cv_;  // mu0_: busy_ == 0
+  std::condition_variable curr_cv_;  // mu1_: current_ == desired_
+  std::deque<Work> work_;            // protected by mu0_
+  IdleFunction idle_;                // protected by mu0_
+  std::size_t min_;                  // protected by mu1_
+  std::size_t max_;                  // protected by mu1_
+  std::size_t desired_;              // protected by mu1_
+  std::size_t current_;              // protected by mu1_
+  std::size_t busy_;                 // protected by mu0_
+  std::size_t done_;                 // protected by mu0_
+  std::size_t caught_;               // protected by mu0_
+  bool corked_;                      // protected by mu0_
 };
 
 }  // anonymous namespace
