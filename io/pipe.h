299e1d9232da18bd9066f961f0068a803c111ecf
Donald King <chronos@chronos-tachyon.net>
2017-01-17 19:41:48 -0800

    Add missing copyright statements.

--- io/pipe.h
+++ io/pipe.h
@@ -1,3 +1,7 @@
+// io/pipe.h - In-process I/O pipes
+// Copyright © 2016 by Donald King <chronos@chronos-tachyon.net>
+// Available under the MIT License. See LICENSE for details.
+
 #ifndef IO_PIPE_H
 #define IO_PIPE_H
 
